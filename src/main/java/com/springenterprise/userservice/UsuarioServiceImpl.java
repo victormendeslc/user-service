e46ea55a0a93d65acdec36b0935b890ee571440f
victor.cunha <victor.cunha@groupsoftware.com.br>
2020-01-10 18:26:38 -0300

    Configuração download nexus

---
+++ src/main/java/com/springenterprise/userservice/UsuarioServiceImpl.java
@@ -0,0 +1,10 @@
+package com.springenterprise.userservice;
+
+import com.spring.enterprise.authserver.commons.UserDTO;
+
+public class UsuarioServiceImpl {
+
+    public void teste(){
+        UserDTO userDTO = new UserDTO();
+    }
+}
